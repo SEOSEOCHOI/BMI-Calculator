a132bb6d2a7564f3c95f3073317972317fbb0137
SEOSEO <82697951+seogyeong1@users.noreply.github.com>
2024-01-05 03:43:50 +0900

    8회차 과제

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -1,10 +1,8 @@
-//
-//  ViewController.swift
-//  BMI Calculator
-//
-//  Created by 최서경 on 1/3/24.
-//
-
+/* 질문 + 수정
+ 1. textField에서의 Outlet Action은 중복 연결이 안 되는지
+ 2. 중복 연결 이후 기존 이벤트가 Did End로 변경되는 이유
+ 3. 탭 제스쳐 이후 텍스트필드 2개의 조건 한번에 비교하기
+ */
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,7 +10,9 @@ class ViewController: UIViewController {
     @IBOutlet var personImageView: UIImageView!
     @IBOutlet var randomCalculateButton: UIButton!
     @IBOutlet var resultButton: UIButton!
+    @IBOutlet var resetButton: UIButton!
     @IBOutlet var inputTextField: [UITextField]!
+    @IBOutlet var nicknameView: UIView!
     
     var BMI: Double = 0
     
@@ -23,50 +23,90 @@ class ViewController: UIViewController {
         textFieldDesign(inputTextField)
         buttonDesign(randomCalculateButton)
         buttonDesign(resultButton)
+        buttonDesign(resetButton)
     }
-    /* 질문사항
-     1. textField에서의 Outlet Action은 중복 연결이 안 되는지
-     2. 중복 연결 이후 기존 이벤트가 Did End로 변경되는 이유
-     3. 탭 제스쳐 이후 텍스트필드 2개의 조건 한번에 비교하기
-     */
-    
+
     
     @IBAction func randomButtonClicked(_ sender: UIButton) {
         let randomHeight: Int = .random(in: 130...200)
         let randomWeight: Int = .random(in: 35...180)
         
-        inputTextField[0].text = "\(randomHeight)"
-        inputTextField[1].text = "\(randomWeight)"
+        inputTextField[1].text = "\(randomHeight)"
+        inputTextField[2].text = "\(randomWeight)"
     }
     
     @IBAction func resultButtonClicked(_ sender: UIButton) {
-        
         worngInput()
-
-        guard let heightText = inputTextField[0].text else { return }
-        guard let weightText = inputTextField[1].text else { return }
+        nicknameInput(nicknameInput: inputTextField[0].text)
+        
+        guard let heightText = inputTextField[1].text else { return }
+        guard let weightText = inputTextField[2].text else { return }
         
         guard let height = Int(heightText) else { return }
         guard let weight = Int(weightText) else { return }
-                
+        
+
+        UserDefaults.standard.set(height, forKey: "height")
+        UserDefaults.standard.set(weight, forKey: "weight")
+        
+
         if standardInput(cm: height, kg: weight) == true {
             let heightMeter: Double = (Double(height) * 0.01)
             
             BMI = Double(Double(weight) / (heightMeter * heightMeter))
             
             calculateBMI(BMI)
+            changeBmiLabel()
         }
     }
-
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        for key in UserDefaults.standard.dictionaryRepresentation().keys {
+            UserDefaults.standard.removeObject(forKey: key.description)
+        }
+        labels[1].text = "당신의 BMI 지수를 알려드릴게요."
+        nicknameView.isHidden = false
+        for textField in inputTextField {
+            textField.text = ""
+        }
+    }
+    
+    func nicknameInput(nicknameInput: String?) {
+        if UserDefaults.standard.string(forKey: "nickname") == nil {
+            if let nickname = nicknameInput {
+                if nickname != "" {
+                    UserDefaults.standard.set(nickname, forKey: "nickname")
+                }
+                
+                if let nicknameText = UserDefaults.standard.string(forKey: "nickname") {
+                    if nicknameText != "" {
+                        nicknameView.isHidden = true
+                        labels[1].text = ("\(nickname)님의 \n키: \(UserDefaults.standard.integer(forKey: "height"))cm 몸무게:\(UserDefaults.standard.integer(forKey: "weight")) \nBMI 지수: \(returnBMIData())")
+                    }
+                }
+            }
+        }
+    }
+    
+    func changeBmiLabel() {
+        if UserDefaults.standard.string(forKey: "nickname") != nil {
+            if let nickname = UserDefaults.standard.string(forKey: "nickname") {
+                if nickname != "" {
+                    nicknameView.isHidden = true
+                    labels[1].text = ("\(nickname)님의 \n키: \(UserDefaults.standard.integer(forKey: "height"))cm 몸무게:\(UserDefaults.standard.integer(forKey: "weight")) \nBMI 지수: \(returnBMIData())")
+                }
+            }
+        }
+    }
     
     func worngInput() {
-        for count in 0 ... 1 {
+        for count in 1 ... 2 {
             if let text = inputTextField[count].text {
                 if text == ""{
                     bmiWaringAlert(message: "빈칸을 채워 주세요.")
                 }
                 
-                if let intText = Int(text) { } else {
+                if Int(text) != nil { } else {
                     bmiWaringAlert(message: "숫자를 입력해 주세요.")
                 }
             }
@@ -89,19 +129,33 @@ class ViewController: UIViewController {
         return true
     }
     
+    func saveBMIData(_ bmiData: String) {
+        UserDefaults.standard.set(bmiData, forKey: "bmiData")
+    }
+    
+    func returnBMIData() -> String {
+        if let bmiData = UserDefaults.standard.string(forKey: "bmiData") {
+            return bmiData
+        }
+        return ""
+    }
+    
     func calculateBMI(_ bmi: Double) {
+        let keyword: String
         switch bmi {
         case 0 ..< 18.5:
-            bmiResultAlert("저체중")
+            keyword = "저체중"
         case 18.5 ..< 23:
-            bmiResultAlert("정상")
+            keyword = "정상"
         case 23 ..< 25:
-            bmiResultAlert("과체중")
+            keyword = "과체중"
         case 25 ..< 30:
-            bmiResultAlert("비만")
+            keyword = "비만"
         default:
-            bmiResultAlert("고도비만")
+            keyword = "고도비만"
         }
+        bmiResultAlert(keyword)
+        saveBMIData(keyword)
     }
     
     func bmiWaringAlert(message warning: String) {
@@ -112,7 +166,9 @@ class ViewController: UIViewController {
     }
     
     func bmiResultAlert(_ bmiResult: String) {
-        let alert = UIAlertController(title: "BMI", message: "\(bmiResult)입니다.", preferredStyle: .alert)
+        let alert = UIAlertController(title: "BMI",
+                                      message: "키: \(UserDefaults.standard.integer(forKey: "height")), 몸무게:\(UserDefaults.standard.integer(forKey: "weight")) \n\(bmiResult)입니다.",
+                                      preferredStyle: .alert)
         let checkButton = UIAlertAction(title: "확인", style: .default)
         alert.addAction(checkButton)
         present(alert, animated: true)
@@ -142,6 +198,12 @@ class ViewController: UIViewController {
             button.setTitleColor(UIColor.white, for: .normal)
             button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
             button.layer.cornerRadius = 15
+        case 3:
+            button.backgroundColor = .systemPink
+            button.setTitle("초기화", for: .normal)
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+            button.layer.cornerRadius = 15
         default:
             break
         }
@@ -156,25 +218,28 @@ class ViewController: UIViewController {
         for label in labels {
             label.font = .systemFont(ofSize: 14)
             label.sizeToFit()
+            
             switch label.tag {
             case 0:
                 label.text = "BMI Calculator"
                 label.font = .boldSystemFont(ofSize: 30)
             case 1:
-                label.text = "당신의 BMI 지수를 \n알려드릴게요."
-                label.numberOfLines = 2
+                if let nickname = UserDefaults.standard.string(forKey: "nickname") {
+                    label.text = ("\(nickname)님의 \n 키: \(UserDefaults.standard.integer(forKey: "height"))cm 몸무게:\(UserDefaults.standard.integer(forKey: "weight")) \nBMI 지수: \(returnBMIData())")
+                    nicknameView.isHidden = true
+                } else {
+                    label.text = "당신의 BMI 지수를 \n알려드릴게요."
+                }
+                label.numberOfLines = 4
             case 2:
-                label.text = "키가 어떻게 되시나요?"
+                label.text = "닉네임을 입력해 주세요"
             case 3:
-                label.text = "몸무게는 어떻게 되시나요?"
+                label.text = "키가 어떻게 되시나요?"
             case 4:
-                label.text = ""
-                label.textAlignment = .center
-                label.textColor = .red
+                label.text = "몸무게는 어떻게 되시나요?"
             default:
                 break
             }
         }
     }
 }
-
